3d6331b0ffee69387c9748e6d09b4aec4b3adb91
SamueleLettieri <samuele2073@gmail.com>
2022-07-19 15:37:09 +0200

    header spotify

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <SpotifyHeader/>
+    <SpotifyMain/>
+  </div>
+</template>
+
+<script>
+import SpotifyHeader from './components/SpotifyHeader.vue';
+import SpotifyMain from './components/SpotifyMain.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SpotifyHeader,
+    SpotifyMain,
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "~bootstrap/scss/bootstrap.scss";
+  
+</style>
---
+++ src/components/SpotifyHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <img class="ms_logo" src="../assets/logo-small.svg" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style lang="scss" scoped>
+header{
+    background-color: rgb(90, 94, 121);
+    padding: 15px;
+}
+
+
+
+.ms_logo{
+    width: 50px;
+}
+
+
+</style>
