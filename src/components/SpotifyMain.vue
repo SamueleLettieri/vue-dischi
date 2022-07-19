aa34ee3d8fe05c86fe1cbf07de488c65b186f625
SamueleLettieri <samuele2073@gmail.com>
2022-07-19 16:04:08 +0200

    errore axios

---
+++ src/components/SpotifyMain.vue
@@ -0,0 +1,55 @@
+<template>
+  <main>
+    <div class="container">
+        <div class="ms_box p-5">
+<!--             <div class="card ms_w">
+                <img src="" class="card-img-top" alt="">
+                <div class="card-body">
+                    <p class="card-text"></p>
+                </div>
+            </div> -->
+        </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return{
+            SpotifyInfo: [],
+        }
+    },
+
+    methods:{
+        getCardsSpotify(){
+            axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+            .then((result) => {
+                consol.log(result.data.results);
+            })
+        }
+    },
+
+    created(){
+        this.getCardsSpotify();
+    }
+}
+</script>
+
+<style lang="scss">
+main{
+   background-color: rgb(27, 32, 46);
+   height: 900px;
+}
+
+.ms_box{
+    display: flex;
+    flex-wrap: wrap;
+    width: calc((100% / 5) );
+}
+
+.ms_w{
+    width: 18rem;
+}
+ 
+</style>
